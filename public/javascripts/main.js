0bf83031cb9f240e4bcc7addc6ff4326bf52e397
atoris1192 <atoris1192@gmail.com>
2018-12-25 17:49:23 +0900

    mongodb

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,28 +2,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict'
 
-  const url = 'http://localhost:3000/api/random'
-
   const btn = document.querySelector('#btn')
-  btn.addEventListener('click', () => {
-      fetch(url)
-        .then(res => {
-          return res.json()
-        })
-        .then(result => {
-          btn.textContent = result.item
-          console.log(result.item)
-        })
-        .catch(err => {
-          console.error(err)
-        })
 
+  btn.addEventListener('click', () => {
+    const url = 'http://localhost:3000/api/random'
+    fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(result => {
+        btn.textContent = result.item
+        console.log(result.item)
+      })
+      .catch(err => {
+        console.error(err)
+      })
   })
+
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pushed')
   })
+
   btn.addEventListener('mouseup', () => {
     btn.classList.remove('pushed')
   })
-  console.log('-- main --')
+
 })
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017/myproject'
+const dbName = 'myproject'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/api/random', (req, res, next) => {
-  const results = ['大吉', '中吉', '末吉', '凶']
-  let random = Math.floor(Math.random() * ( 1+results.length ))
-  let item = results[random]
-  const data = { item }
-  res.json(data)
-})
+
+const main = async () => {
+  const client = new MongoClient(url, { useNewUrlParser: true })
+  await client.connect()
+  console.info("Connected  Server ...")
+  const db = client.db(dbName)
+
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+  router.get('/api/insertMany', async(req, res, next) => {
+    const collection = db.collection('omikuji')
+    await collection.insertMany([
+      {"item": "大吉"},
+      {"item": "中吉"},
+      {"item": "末吉"},
+      {"item": "凶"},
+    ])
+      .then(res => console.log(res.result))
+      .catch(err => console.error(err))
+    res.send('insertMany done')
+  })
+  router.get('/api/deleteMany', async(req, res) => {
+    const collection = db.collection('omikuji')
+    await collection.deleteMany('omikuji')
+      .then(res => console.log(res.result))
+      .catch(err => console.error(err))
+    res.send('DeleteMany done')
+  })
+
+  router.get('/api/random', async(req, res, next) => {
+    const collection = db.collection('omikuji')
+    const result = await collection.aggregate([
+      { $sample: { size: 1 }}
+    ]).toArray()
+    console.log(result[0])
+    res.json(result[0])
+  })
+  
+}
+main()
+
 
 module.exports = router;
