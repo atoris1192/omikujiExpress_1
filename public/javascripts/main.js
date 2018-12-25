732d9fd712630d86af0a575574521ad676dbaa97
atoris1192 <atoris1192@gmail.com>
2018-12-25 09:44:38 +0900

    06

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const btn = document.querySelector('#btn')
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!'
+    let random = Math.floor(Math.random() * (1+2))
+    switch(random) {
+      case 0:
+      btn.textContent = "大吉"
+      break
+      case 1:
+      btn.textContent = "中吉"
+      break
+      case 2:
+      btn.textContent = "凶"
+      break
+    }
+    // btn.textContent = random
   })
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pushed')
